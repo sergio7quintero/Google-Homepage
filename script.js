8d52c9b61515840d2934ffc0d6cc27dd1138a4d7
sergio7quintero <yuyisq@gmail.com>
2024-10-06 15:08:07 -0400

    Finished product
    added the google images page

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let searchBar = document.getElementById("search-bar")
+let searchBar = document.getElementById("search-bar") //searchBar goes into my html document and finds an id called "search-bar"
 
-searchBar.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const inputs = document.getElementById('q')
-    if (inputs.value === "") {
-        alert ("PLease enter something")
+searchBar.addEventListener("submit", (event) => { //addEventListener is looking for a command, in this case a click or "submit" or "enter"
+    event.preventDefault(); //this will stop the form from doing what it normally does
+    const googleq = document.getElementById('q') //googleq goes into my document and finds an id called q, its q becasue google search starts with q
+    if (googleq.value === "") { //if you search null then it gives you an alert
+        alert ("Please type something")
     }
-    else {
-        window.location.href = "https://google.com/search?q=" + inputs.value;
+    else { //it'll send you to the url + whatever the user searches... it'll send you to the page the user searches
+        window.location.href = "https://google.com/search?q=" + googleq.value;
     }
 })
